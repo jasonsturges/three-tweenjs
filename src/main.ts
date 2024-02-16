11b8985ed39bcf78e97158d169f3147c31a666d8
Jason Sturges <jason@jasonsturges.com>
2024-02-15 23:28:57 -0600

    Main app

--- src/main.ts
+++ src/main.ts
@@ -2,30 +2,33 @@ import "./style.css";
 import {
   BoxGeometry,
   CineonToneMapping,
+  Clock,
   DirectionalLight,
+  HemisphereLight,
   Mesh,
   MeshStandardMaterial,
+  Object3D,
   PCFSoftShadowMap,
   PerspectiveCamera,
-  PlaneGeometry,
   Scene,
+  Vector3,
   WebGLRenderer,
 } from "three";
-import { OrbitControls } from "three-stdlib";
+import * as TWEEN from "@tweenjs/tween.js";
 
-// Create a scene
+// Scene
 const scene = new Scene();
 
-// Create a camera
+// Camera
 const camera = new PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
-  0.1,
-  1000,
+  50,
+  10000,
 );
 camera.position.z = 5;
 
-// Create a renderer
+// Renderer
 const renderer = new WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setClearColor(0x090909);
@@ -34,38 +37,84 @@ renderer.shadowMap.type = PCFSoftShadowMap;
 renderer.toneMapping = CineonToneMapping;
 document.body.appendChild(renderer.domElement);
 
-// Create a cube
-const geometry = new BoxGeometry();
-const material = new MeshStandardMaterial({ color: 0x00ff00 });
-const cube = new Mesh(geometry, material);
-cube.castShadow = true;
-scene.add(cube);
+const count = 64;
 
-// Create a plane
-const groundGeometry = new PlaneGeometry(10, 10);
-const groundMaterial = new MeshStandardMaterial({ color: 0x808080 });
-groundMaterial.roughness = 0.6;
-groundMaterial.metalness = 0.4;
-const ground = new Mesh(groundGeometry, groundMaterial);
-ground.rotation.x = -Math.PI / 2;
-ground.position.y = -1;
-ground.receiveShadow = true;
-scene.add(ground);
+// Materials
+const materials: MeshStandardMaterial[] = [];
+let f = 0xff / count;
+for (let i = 0; i < count; i++) {
+  const color = ((i * f) << 16) | ((i * f) << 8) | (i * f);
+  materials.push(new MeshStandardMaterial({ color: color }));
+}
 
-// Add a light
-const light = new DirectionalLight(0xffffff, 1);
-light.position.set(0, 5, 0);
-light.castShadow = true;
-scene.add(light);
+// Objects
+const models: Mesh<BoxGeometry, MeshStandardMaterial>[] = [];
+const geometry = new BoxGeometry(20, 20, 20);
+for (let i = 0; i < count; i++) {
+  const material = materials[i];
+  const cube = new Mesh(geometry, material);
+  models.push(cube);
+  scene.add(cube);
+}
 
-// Add orbit controls
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.update();
+// Lights
+const ambientLight = new DirectionalLight(0x9090aa);
+ambientLight.position.set(-100, -300, -500).normalize();
+scene.add(ambientLight);
 
-// Animation loop
+const hemisphereLight = new HemisphereLight(0xffffff, 0x444444);
+hemisphereLight.position.set(1, 1, 1);
+scene.add(hemisphereLight);
+
+// Animation
+let timer = 0;
+const clock = new Clock();
 renderer.setAnimationLoop(() => {
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  const time = clock.getElapsedTime();
+  if (clock.elapsedTime > timer) {
+    timer = clock.elapsedTime + Math.random() + 0.25;
+    const time = (timer - clock.elapsedTime) * 1000 - 150;
+    const sx = Math.random() * 3;
+    const sy = Math.random() * 3;
+    const sz = Math.random() * 2;
+    const yaw = Math.random() * 1000;
+    const pitch = Math.random() * 1200;
+    const dist = Math.random() * 500 + 250;
+    const lookAtZero = true;
+
+    for (let i = 0; i < models.length; i++) {
+      const model = models[i];
+      const n = i / models.length - 0.5;
+      const t = new Object3D();
+
+      t.rotateX((pitch * n * Math.PI) / 180);
+      t.rotateY((yaw * n * Math.PI) / 180);
+      t.translateZ(dist);
+      t.scale.set(sx, sy, sz);
+
+      if (lookAtZero) {
+        t.lookAt(new Vector3(1, 0, 0));
+      }
+
+      new TWEEN.Tween(model.position)
+        .to(t.position, time)
+        .easing(TWEEN.Easing.Quadratic.InOut)
+        .start();
+      new TWEEN.Tween(model.scale)
+        .to(t.scale, time)
+        .easing(TWEEN.Easing.Quadratic.InOut)
+        .start();
+      new TWEEN.Tween(model.rotation)
+        .to(t.rotation, time)
+        .easing(TWEEN.Easing.Quadratic.InOut)
+        .start();
+    }
+  }
+
+  camera.position.set(Math.sin(time) * 1000, 20, Math.cos(time) * 1000);
+  camera.lookAt(new Vector3(0, 0, 0));
+
+  TWEEN.update();
 
   renderer.render(scene, camera);
 });
